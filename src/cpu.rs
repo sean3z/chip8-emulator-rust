282bc81356ddb9df31d52eabcdadd3fe016305c7
Sean Wragg <sean@lappy486.cfl.rr.com>
2018-04-23 04:58:30 -0400

    priv8 vars; start process opcode method

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,18 @@
-
 pub struct Cpu {
-    pub pc: u16, // program counter
-    pub stack: [u16; 16],
+    pc: u16, // program counter
+    stack: [u16; 16],
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            pc: 0x200,
+            stack: [0; 16]
+        }
+    }
+
+    pub fn process(&mut self, memory: [u8; 4096]) {
+        let opcode: u16 = memory[self.pc as usize] as u16;
+        self.pc += 2;
+    }
 }
